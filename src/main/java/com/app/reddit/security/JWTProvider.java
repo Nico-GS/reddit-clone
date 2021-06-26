7ac4e3fbaef617cb49dac105f4b09ba0029c9dff
Nico-GS <ng73800@outlook.fr>
2021-06-26 16:19:19 +0200

    Service / Controller / Config / Mail / Token Auth

--- src/main/java/com/app/reddit/security/JWTProvider.java
+++ src/main/java/com/app/reddit/security/JWTProvider.java
@@ -1,23 +1,24 @@
 package com.app.reddit.security;
 
 import com.app.reddit.exception.ActivationException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
 
+import static io.jsonwebtoken.Jwts.parserBuilder;
+
 @Service
 public class JWTProvider {
-    private KeyStore keystore;
 
+    private KeyStore keystore;
 
     @PostConstruct
     public void init() {
@@ -35,6 +36,11 @@ public class JWTProvider {
         return Jwts.builder().setSubject(princ.getUsername()).signWith(getPrivKey()).compact();
     }
 
+    public boolean validateToken (String token) {
+        parserBuilder().setSigningKey(getPubKey()).build().parseClaimsJws(token);
+        return true;
+    }
+
     // modifier alias / refaire keystore
     private PrivateKey getPrivKey () {
         try {
@@ -42,5 +48,22 @@ public class JWTProvider {
         } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
             throw new ActivationException("Exception occurred while retrieving public key");
         }
+    }
+
+    private PublicKey getPubKey () {
+        try {
+            return keystore.getCertificate("vox").getPublicKey();
+        } catch(KeyStoreException e) {
+            throw new ActivationException("Exception occurred retrieving public key");
+        }
+    }
+
+    public String getNameFromJWT(String token) {
+        Claims claims = parserBuilder()
+                .setSigningKey(getPubKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
     }
 }
